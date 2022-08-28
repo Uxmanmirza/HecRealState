c294a5de04c2af469996d4726af461cd355e24b8
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2022-08-28 13:15:03 +0500

    first commit

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default function AppNavigator() {
+  return (
+     <NavigationContainer>
+        <View>
+            <Text>Its mocjdv kad kd</Text></View>
+     </NavigationContainer>
+  )
+}
